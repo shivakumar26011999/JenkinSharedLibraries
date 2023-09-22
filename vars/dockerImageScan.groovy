e5ca14083cccaff4e2163fa0e9f7e7d86ab87f2e
shiva <gandheshiva9@gmail.com>
2023-09-22 09:09:25 +0530

    comments added for docker image scan

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,9 @@
 def call(String appName, String appVersion, String hubUser) {
     sh """
+      echo "-------- Docker Image Scan --------"
       cat ${hubUser}
       trivy image ${appName}:${appVersion} > scan.txt
       cat scan.txt
+      echo "-------- Docker Image Scan Complete --------"
     """
 }
