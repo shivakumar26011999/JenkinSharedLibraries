247a1c5eee77cb115d68f11e4b9af14f7b45fb87
shiva <gandheshiva9@gmail.com>
2023-09-22 08:32:50 +0530

    library for docker image build added.

--- vars/dockerImageBuild.groovy
+++ vars/dockerImageBuild.groovy
@@ -1,8 +1,8 @@
 def call(String appName, String appVersion, String commonVersion) {
     sh """
         echo "-------- Building Docker Image --------"
-        docker build -t ${appName}:${appVersion}
-        docker build -t ${appName}:${commonVersion}
+        docker build -t ${appName}:${appVersion} .
+        docker build -t ${appName}:${commonVersion} .
         echo "-------- Building Docker Image Complete --------"
     """
 }
